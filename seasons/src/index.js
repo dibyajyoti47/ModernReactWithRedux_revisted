24aebbcbfbe1e847388f1b45c57b0a30ec05f359
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2021-05-31 23:43:13 +0530

    section 6 completed.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,33 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import SeasonDisplay from "./SeasonDisplay";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 if (module.hot) {
   module.hot.accept();
 }
 
 class App extends React.Component {
-  constructor(props, context) {
-    super(props);
-    this.state = { lat: null, errorMessage: "" };
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (error) => {
-        this.setState({ errorMessage: error.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
     );
   }
 
-  render() {
+  componentDidUpdate() {
+    console.log("My component was just - it rerendred.");
+  }
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div> latitude: {this.state.lat}</div>;
+      return <SeasonDisplay latitude={this.state.lat} />;
     }
-    return <div>Loading...</div>;
+    return <Spinner message="Please accept the request" />;
+  }
+  render() {
+    return this.renderContent();
   }
 }
 
