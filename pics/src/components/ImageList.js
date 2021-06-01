091831f2246c83cf7a1271d8a7a50744e7d2f85e
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2021-06-01 12:25:36 +0530

    section 8 & 9 completed

---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ImageList = props => {
+  const images = props.images.map(({ urls, id, description }) => {
+    return <img src={urls.regular} alt={description} key={id} />;
+  });
+  return <div>{images}</div>;
+};
+
+export default ImageList;
