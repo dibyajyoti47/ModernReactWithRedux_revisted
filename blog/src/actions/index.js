15d08c959dffcb063f1cdd132e1c6c86e912609e
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2021-06-04 23:46:30 +0530

    section 19 completed.

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,14 @@
 import _ from "lodash";
 import jsonplaceholder from "../apis/jsonplaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(userId => {
+    dispatch(fetchUser(userId));
+  });
+};
+
 export const fetchPosts = () => {
   return async dispatch => {
     const response = await jsonplaceholder.get("/posts");
@@ -8,9 +16,17 @@ export const fetchPosts = () => {
   };
 };
 
+export const fetchUser = id => async dispatch => {
+  const response = await jsonplaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+/*  using memoize
 export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 
 const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonplaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
 });
+
+*/
